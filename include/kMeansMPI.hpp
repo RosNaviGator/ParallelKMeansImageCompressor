a962710fa07866c1e912fc33475fbf0d277ecbb6
leonardopagliochini <leonardoignazio.pagliochini@mail.polimi.it>
2024-05-15 00:19:54 +0200

    Replaced the kMeansMPI.hpp

--- include/kMeansMPI.hpp
+++ include/kMeansMPI.hpp
@@ -15,21 +15,24 @@
 
 class KMeans
 {
-public:
-    KMeans(const int& k, const std::vector<Point> points);
+    public:
+        KMeans(const int& k, const int& rank, const int& n_features,  const std::vector<Point> points);    
+        KMeans(const int& k, const int& rank, const int& n_features);
+        
 
-    void run(const int& rank, const int& world_size);
-    void printClusters() const;
-    void plotClusters();
-    std::vector<Point> getPoints();
-    std::vector<Point> getCentroids();
-    int getNumberOfIterationForConvergence();
-    int numberOfIterationForConvergence;
-private:
-    
-    int k;
-    std::vector<Point> points;
-    std::vector<Point> centroids;
+        void run(const int& rank, const int& world_size, const std::vector<std::pair<int, Point> > local_points);
+        void printClusters() const;
+        void plotClusters();
+        std::vector<Point> getPoints();
+        std::vector<Point> getCentroids();
+        int getNumberOfIterationForConvergence();
+        int numberOfIterationForConvergence;
+
+    private:
+        
+        int k;
+        std::vector<Point> points;
+        std::vector<Point> centroids;
     // Gnuplot gp;
 };
 
